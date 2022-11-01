9e813d8b3e46f6df6ac5852c46ceabfed1eb80a5
Andrew <80355579+Alekseevich-psk@users.noreply.github.com>
2022-11-01 14:33:10 +0300

    init project

---
+++ src/js/components/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                test1
+            </div>
+        );
+    }
+}
+
+export default App;
